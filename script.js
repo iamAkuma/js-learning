7fcf7362eeed6cad35bb9d1301b1f323bad9ce28
Saral Bajracharya <rmanbajra@gmail.com>
2024-01-21 18:31:29 +0545

    Basic JS

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-let a = "HELLO"
-let b = "BROTHER"
+// // let a = "HELLO"
+// // let b = "BROTHER"
 
-console.log(a + " " + b)
+// // console.log(a + " " + b)
+
+// let a = true
+// let b = false
+// console.log(b && a)
+
+let a = 1;
+let b = 2;
+
+console.log(a != b)
