a7d80bf193d3501f6ab95552afe6ee502a69b83b
Saral Bajracharya <rmanbajra@gmail.com>
2024-03-19 19:29:36 +0545

    Reference vs Value

--- script.js
+++ script.js
@@ -1,197 +1,208 @@
-// // // // // // // // // // // // // let a = "HELLO"
-// // // // // // // // // // // // // let b = "BROTHER"
+// // // // // // // // // // // // // // let a = "HELLO"
+// // // // // // // // // // // // // // let b = "BROTHER"
 
-// // // // // // // // // // // // // console.log(a + " " + b)
+// // // // // // // // // // // // // // console.log(a + " " + b)
 
-// // // // // // // // // // // // let a = true
-// // // // // // // // // // // // let b = false
-// // // // // // // // // // // // console.log(b && a)
+// // // // // // // // // // // // // let a = true
+// // // // // // // // // // // // // let b = false
+// // // // // // // // // // // // // console.log(b && a)
 
-// // // // // // // // // // // // let a = 1;
-// // // // // // // // // // // // let b = 2;
+// // // // // // // // // // // // // let a = 1;
+// // // // // // // // // // // // // let b = 2;
 
-// // // // // // // // // // // // console.log(a != b)
+// // // // // // // // // // // // // console.log(a != b)
 
 
-// // // // // // // // // // // function sayHello() {
-// // // // // // // // // // //     console.log('Hello')
+// // // // // // // // // // // // function sayHello() {
+// // // // // // // // // // // //     console.log('Hello')
 
+// // // // // // // // // // // // }
+// // // // // // // // // // // // sayHello();
+
+
+// // // // // // // // // // // // function sum(a, b) {
+
+// // // // // // // // // // // //     console.log(a + b)
+// // // // // // // // // // // // }
+
+// // // // // // // // // // // // sum(40, 29)
+
+
+// // // // // // // // // // // // function sayName(name) {
+// // // // // // // // // // // //     console.log(name)
+// // // // // // // // // // // // }
+
+// // // // // // // // // // // // sayName('Tero Dai')
+
+
+// // // // // // // // // // // function sum1(a, b) {
+// // // // // // // // // // //     return a + b
 // // // // // // // // // // // }
-// // // // // // // // // // // sayHello();
 
+// // // // // // // // // // // let s = sum1(40, 29)
+// // // // // // // // // // // console.log(s)
 
-// // // // // // // // // // // function sum(a, b) {
+// // // // // // // // // // function sayName(name) {
+// // // // // // // // // //     return "Hello  " + name
+// // // // // // // // // // }
 
-// // // // // // // // // // //     console.log(a + b)
-// // // // // // // // // // // }
+// // // // // // // // // // let n = sayName('Saral')
+// // // // // // // // // // console.log(n)
 
-// // // // // // // // // // // sum(40, 29)
+// // // // // // // // // //Arrow Functions
 
-
-// // // // // // // // // // // function sayName(name) {
-// // // // // // // // // // //     console.log(name)
-// // // // // // // // // // // }
-
-// // // // // // // // // // // sayName('Tero Dai')
-
-
-// // // // // // // // // // function sum1(a, b) {
+// // // // // // // // // // let sumArrow = (a, b) => {
 // // // // // // // // // //     return a + b
 // // // // // // // // // // }
 
-// // // // // // // // // // let s = sum1(40, 29)
-// // // // // // // // // // console.log(s)
+// // // // // // // // // // console.log(sumArrow(1, 2))
 
-// // // // // // // // // function sayName(name) {
-// // // // // // // // //     return "Hello  " + name
+// // // // // // // // // // let printName = (name, age) => {
+// // // // // // // // // //     console.log(name)
+// // // // // // // // // //     console.log(age)
+// // // // // // // // // // }
+// // // // // // // // // // printName("Saral", 22)
+
+// // // // // // // // // // let sumArrow = (a, b) => a + b
+// // // // // // // // // // console.log(sumArrow(40, 29))
+
+// // // // // // // // // // let printName = (name) => "Hi " + name
+// // // // // // // // // // console.log(printName('Saral'))
+
+// // // // // // // // // // let hi = () => 'HI'
+// // // // // // // // // // console.log(hi())
+
+// // // // // // // // // //Stack Tracing and Call Stack
+
+// // // // // // // // // function doStuff(a, b, name) {
+// // // // // // // // //     print(sum(a, b))
+// // // // // // // // //     print(sayHi(name))
 // // // // // // // // // }
 
-// // // // // // // // // let n = sayName('Saral')
-// // // // // // // // // console.log(n)
+// // // // // // // // // function print(variable) {
+// // // // // // // // //     console.log(variable)
+// // // // // // // // // }
 
-// // // // // // // // //Arrow Functions
-
-// // // // // // // // // let sumArrow = (a, b) => {
+// // // // // // // // // function sum(a, b) {
+// // // // // // // // //     asdasd
 // // // // // // // // //     return a + b
 // // // // // // // // // }
 
-// // // // // // // // // console.log(sumArrow(1, 2))
-
-// // // // // // // // // let printName = (name, age) => {
-// // // // // // // // //     console.log(name)
-// // // // // // // // //     console.log(age)
+// // // // // // // // // function sayHi(name) {
+// // // // // // // // //     return "Hi " + name
 // // // // // // // // // }
-// // // // // // // // // printName("Saral", 22)
 
-// // // // // // // // // let sumArrow = (a, b) => a + b
-// // // // // // // // // console.log(sumArrow(40, 29))
+// // // // // // // // // doStuff(1, 2, "Saral")
 
-// // // // // // // // // let printName = (name) => "Hi " + name
-// // // // // // // // // console.log(printName('Saral'))
+// // // // // // // // //Hoisting
+// // // // // // // // //Takes functions to the top
+// // // // // // // // //even if defined in the bottom
+// // // // // // // // //which is called hoisting
+// // // // // // // // // doesnot work in arrow functions
 
-// // // // // // // // // let hi = () => 'HI'
-// // // // // // // // // console.log(hi())
+// // // // // // // // console.log(sumArrow(1, 2))
 
-// // // // // // // // //Stack Tracing and Call Stack
+// // // // // // // // // function sum(a, b) {
+// // // // // // // // //     return a + b
+// // // // // // // // // }
 
-// // // // // // // // function doStuff(a, b, name) {
-// // // // // // // //     print(sum(a, b))
-// // // // // // // //     print(sayHi(name))
+// // // // // // // // let sumArrow = (a, b) => {
+// // // // // // // //     a + b
 // // // // // // // // }
 
-// // // // // // // // function print(variable) {
-// // // // // // // //     console.log(variable)
-// // // // // // // // }
 
-// // // // // // // // function sum(a, b) {
-// // // // // // // //     asdasd
-// // // // // // // //     return a + b
-// // // // // // // // }
-
-// // // // // // // // function sayHi(name) {
-// // // // // // // //     return "Hi " + name
-// // // // // // // // }
-
-// // // // // // // // doStuff(1, 2, "Saral")
-
-// // // // // // // //Hoisting
-// // // // // // // //Takes functions to the top
-// // // // // // // //even if defined in the bottom
-// // // // // // // //which is called hoisting
-// // // // // // // // doesnot work in arrow functions
-
-// // // // // // // console.log(sumArrow(1, 2))
-
-// // // // // // // // function sum(a, b) {
-// // // // // // // //     return a + b
-// // // // // // // // }
-
-// // // // // // // let sumArrow = (a, b) => {
-// // // // // // //     a + b
+// // // // // // // function sayHi(name) {
+// // // // // // //     let result = "Hi " + name
+// // // // // // //     console.log(result)
 // // // // // // // }
 
+// // // // // // // // () = > {
+// // // // // // // // esbhitra vako chai chuttai scope huncha
+// // // // // // // // so same variable huda pani it is different from the other result
+// // // // // // // // }
+// // // // // // // let result = 'Saral!'
 
-// // // // // // function sayHi(name) {
-// // // // // //     let result = "Hi " + name
-// // // // // //     console.log(result)
+// // // // // // // sayHi(result)
+
+
+// // // // // // //Closures
+
+// // // // // // function print(variable) {
+// // // // // //     let c = 3
+// // // // // //     return function func(variable2) {
+// // // // // //         console.log(variable)
+// // // // // //         console.log(variable2)
+// // // // // //         console.log(c)
+// // // // // //     }
 // // // // // // }
 
-// // // // // // // () = > {
-// // // // // // // esbhitra vako chai chuttai scope huncha
-// // // // // // // so same variable huda pani it is different from the other result
-// // // // // // // }
-// // // // // // let result = 'Saral!'
-
-// // // // // // sayHi(result)
+// // // // // // let a = print(1)
+// // // // // // a(2)
 
 
-// // // // // //Closures
+// // // // // //const keyword
+// // // // // let a = 1
+// // // // // const b = 2
+// // // // // a = 2
+// // // // // // b = 3 //gives error because the const(constant) variable can't be changed
+// // // // // // variable lai change nagarauna laai const use garincha
 
-// // // // // function print(variable) {
-// // // // //     let c = 3
-// // // // //     return function func(variable2) {
-// // // // //         console.log(variable)
-// // // // //         console.log(variable2)
-// // // // //         console.log(c)
-// // // // //     }
-// // // // // }
+// // // // // console.log(a)
+// // // // // console.log(b)
 
-// // // // // let a = print(1)
-// // // // // a(2)
+// // // // //var keyword
 
+// // // // // var a = 1
+// // // // // a = 2
+// // // // // console.log(a)
 
-// // // // //const keyword
-// // // // let a = 1
-// // // // const b = 2
-// // // // a = 2
-// // // // // b = 3 //gives error because the const(constant) variable can't be changed
-// // // // // variable lai change nagarauna laai const use garincha
-
-// // // // console.log(a)
-// // // // console.log(b)
-
-// // // //var keyword
-
-// // // // var a = 1
-// // // // a = 2
+// // // // {
+// // // //     var a = 1
+// // // // }
 // // // // console.log(a)
 
-// // // {
-// // //     var a = 1
-// // // }
-// // // console.log(a)
+// // // // //var breaks scoping rules,
+// // // // // curly braces bhitra declare gareko lai pani bahira console log gardincha
+// // // // //better not to use var
 
-// // // //var breaks scoping rules,
-// // // // curly braces bhitra declare gareko lai pani bahira console log gardincha
-// // // //better not to use var
+// // // // Type Coercion
+// // // // converting types, like in C#
 
-// // // Type Coercion
-// // // converting types, like in C#
+// // // let a = 1
+// // // const b = "1"
 
-// // let a = 1
-// // const b = "1"
-
-// // console.log(b == a) //gives true because JS converts them to same type :3
-// // // console.log(typeof parseInt(a))
-// // // console.log(typeof a)
+// // // console.log(b == a) //gives true because JS converts them to same type :3
+// // // // console.log(typeof parseInt(a))
+// // // // console.log(typeof a)
 
 
-// // const a = 1
-// // const b = "1"
+// // // const a = 1
+// // // const b = "1"
 
-// // console.log(a === b) // use === to compare the exact types
-// // type coercion doesn't happen while using
+// // // console.log(a === b) // use === to compare the exact types
+// // // type coercion doesn't happen while using
 
-// const a = null
-// const b = undefined
+// // const a = null
+// // const b = undefined
 
-// console.log(a == b)
-// // null, undefined ma chai feri "==" use gardaa true output dincha ani "===" garda false
-// // so better to use "==" in this case
-
-
-//ARRAYS
-const a = [[1, 0], 2, 3, 4, 5]
+// // console.log(a == b)
+// // // null, undefined ma chai feri "==" use gardaa true output dincha ani "===" garda false
+// // // so better to use "==" in this case
 
 
-console.log(a[0])
+// //ARRAYS
+// const a = [[1, 0], 2, 3, 4, 5]
+
+
+// console.log(a[0])
+
+//Reference vs Value
+let a = 10
+let b = "Hi"
+let c = [1, 2]
+let d = c
+d.push(3)
+console.log(c)
+console.log(d)
+
+//interesting
